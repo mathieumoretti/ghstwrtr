ec9d2e37dacbde2b4f2c7afc0ccbbba173beba3c
Mathieu Moretti <mormmorm@hotmail.com>
2018-11-23 23:34:08 -0500

    travis unit testing framework setup

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 server.listen(PORT, () => {
+  // eslint-disable-next-line
   console.log(`Server running on ${PORT}/`);
 });
