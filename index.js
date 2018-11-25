396b5c5c257b69403b1af5e2a54d63f86d6abe39
mathieumoretti <mormmorm@hotmail.com>
2018-11-24 22:54:58 -0500

    port fix (#11)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 // routes
 app.get('/', (req, res) => {
   res.send('hello world!');
@@ -11,4 +11,4 @@ app.get('/stories', (req, res) => {
   res.send('stories');
 });
 
-app.listen(5000);
+app.listen(PORT);
