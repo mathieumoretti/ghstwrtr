173957398615d4754d84d739c4e822e36b2d5538
Mathieu Moretti <mormmorm@hotmail.com>
2018-11-24 22:39:23 -0500

    Lint fix

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-//express hello world
-var express = require('express');
-var app = express();
+// express hello world
+const express = require('express');
+
+const app = express();
 
 // routes
-app.get('/', (req, res) =­>
-{
-   res.send("hello world!");
+app.get('/', (req, res) => {
+  res.send('hello world!');
 });
-app.get('/stories', function(req, res){
-   res.send("stories!");
+app.get('/stories', (req, res) => {
+  res.send('stories');
 });
 
 app.listen(5000);
