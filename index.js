ac65ec0091398889540a22315ede850811557fca
mathieumoretti <mormmorm@hotmail.com>
2018-11-27 00:26:32 -0500

    Hello world pug (#13)
    * Hello world pug
    
    * Lint fix

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// views
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 // routes
 app.get('/', (req, res) => {
-  res.send('hello world!');
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 app.get('/stories', (req, res) => {
   res.send('stories');
