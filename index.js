e0868518157bdb6089c604f9fcf98449323bdda8
mathieumoretti <mormmorm@hotmail.com>
2018-11-28 20:58:56 -0500

    Install bootstrap (#14)
    * install_bootstrap
    
    * login template
    
    * Couple more views
    
    * Lint fixes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // express hello world
 const express = require('express');
+const console = require('console');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,12 +9,17 @@ const PORT = process.env.PORT || 5000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+
 // routes
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' });
+  res.render('index', { title: 'ghstwrtr' });
+});
+app.get('/login', (req, res) => {
+  res.render('login', { title: 'ghstwrtr' });
 });
 app.get('/stories', (req, res) => {
   res.send('stories');
 });
 
 app.listen(PORT);
+console.log('Server started');
