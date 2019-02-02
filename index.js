5c00e314646586ab44a4758d086225795e2e24e0
Mathieu Moretti <mormmorm@hotmail.com>
2019-02-02 13:52:08 -0500

    Fix lint problems + sub lorem for bible beginning

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // express hello world
 const express = require('express');
 const console = require('console');
-var fs = require('fs');
-var _ = require('underscore');
- 
+const fs = require('fs');
+const path = require('path');
+// const _ = require('underscore');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,8 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-//app.use('/static', express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 // routes
 app.get('/', (req, res) => {
   res.render('index', { title: 'ghstwrtr' });
@@ -43,113 +43,104 @@ app.get('/login', (req, res) => {
 // adv:
 // slang: #YOLO
 
-var content = [];
-var stories = [];
+let content = [];
+let stories = [];
 
-function generateContent()
-{
-    var sentences;
-    var fileContent = fs.readFileSync('data.txt', 'utf8');   
-    return fileContent.split(".");
+function generateContent() {
+  const fileContent = fs.readFileSync('data.txt', 'utf8');
+  return fileContent.split('.');
 }
 
-content = generateContent()
+content = generateContent();
 
-function random()
-{
-    var d = new Date();
-    var n = d.getTime();
-    return Math.random() * n;
+function random() {
+  const d = new Date();
+  const n = d.getTime();
+  return Math.random() * n;
 }
 
-function randomCell(arr)
-{
-    return arr[Math.floor(random() * arr.length) % arr.length];
+function randomCell(arr) {
+  return arr[Math.floor(random() * arr.length) % arr.length];
 }
 
-function makeContent(noOfSentences)
-{ 
-    var sentences = [];
-    for (var i = 0; i < noOfSentences; i++) {        
-        sentences[i] = randomCell(content);
-    }
-    return sentences;
+function makeContent(noOfSentences) {
+  const sentences = [];
+  for (let i = 0; i < noOfSentences; i += 1) { // filter verses
+    sentences[i] = randomCell(content);
+    sentences[i] = `${sentences[i]}.`;
+    sentences[i] = sentences[i].replace(/[\d+:\d+]/g, '');
+  }
+  return sentences;
 }
 
-function makeHeadline()
-{
-    return 'Lorem Ipsum';
+function makeHeadline() {
+  return 'Lorem Ipsum';
 }
 
-var adjective = ["Flying", "Cool", "Funny"];
+const adjective = ['Flying', 'Cool', 'Funny'];
 
-function makeAuthors()
-{
-    return randomCell(adjective) + " Ghost " + Math.floor(100 * random() % 100);
+function makeAuthors() {
+  return `${randomCell(adjective)} Ghost ${Math.floor((100 * random()) % 100)}`;
 }
 
-var storyCounter = 0;
+let storyCounter = 0;
 
-function makeStory(noOfSentences)
-{
-    var story = {
-        id : storyCounter++,
-        headline : makeHeadline(),
-        authors : makeAuthors(),
-        content : makeContent(noOfSentences)
-    };
+function makeStory(noOfSentences) {
+  const story = {
+    id: storyCounter += 1,
+    headline: makeHeadline(),
+    authors: makeAuthors(),
+    content: makeContent(noOfSentences),
+  };
 
-    return story;
+  return story;
 }
 
-function makeStories(noOfstories)
-{
-    var stories = [];
-    for (var i = 0; i < noOfstories; i++) {
-        stories[i] = makeStory(10)
-    }
-    return stories;
+function makeStories(noOfstories) {
+  for (let i = 0; i < noOfstories; i += 1) {
+    stories[i] = makeStory(10);
+  }
+  return stories;
 }
 
 function formatDate(date) {
-  var monthNames = [
-    "January", "February", "March",
-    "April", "May", "June", "July",
-    "August", "September", "October",
-    "November", "December"
+  const monthNames = [
+    'January', 'February', 'March',
+    'April', 'May', 'June', 'July',
+    'August', 'September', 'October',
+    'November', 'December',
   ];
 
-  var day = date.getDate();
-  var monthIndex = date.getMonth();
-  var year = date.getFullYear();
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const year = date.getFullYear();
 
-  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+  return `${day}  ${monthNames[monthIndex]} ${year}`;
 }
 
-var date = formatDate(new Date());
-console.log(date);
+const theDate = formatDate(new Date());
+console.log(theDate);
 
-var stories = makeStories(10);
-var newspaper = 
-{ 
-    model: 
-    { 
-        title: 'ghstwrtr',
-        date: date,
-        // main story
-        mainStory: stories[0],
-        // list of stories
-        secondaryStories: stories.slice(1, stories.length - 1), 
-    } 
-}
+stories = makeStories(10);
+const newspaper = {
+  model:
+  {
+    title: 'ghstwrtr',
+    date: theDate,
+    // main story
+    mainStory: stories[0],
+    // list of stories
+    secondaryStories: stories.slice(1, stories.length - 1),
+  },
+};
 
 app.get('/stories', (req, res) => {
   res.render('stories', newspaper);
 });
 
 app.get('/story/:storyId', (req, res) => {
-    var story = (req.params.storyId) ? stories[req.params.storyId] : stories[0]
-    res.render('story', story);
+  const story = (req.params.storyId) ? stories[req.params.storyId] : stories[0];
+  res.render('story', story);
 });
 
 app.listen(PORT);
