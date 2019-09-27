c74786abd4f817b0e955ffa435463443724b723b
Mathieu Moretti <mormmorm@hotmail.com>
2019-09-26 21:44:04 -0400

    Hello world postgres + bootstrap stuff

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,12 +1,40 @@
 const express = require('express');
 var router = express.Router();
 
+var pg = require("pg");
+var app = express();
+ 
+const config = {
+  user: 'postgres',
+  database: 'ghstwrtr',
+  port: 5432
+};
+var pool = new pg.Pool(config)
 router.get('/', (req, res) => {
-    res.render('index', { title: 'ghstwrtr' });
+  pool.connect(function(err, client, done) {
+    if(err){
+        console.log("not able to get connection "+ err);
+        res.status(400).send(err);
+    } 
+    client.query('SELECT * FROM student where id = $1', [1],function(err,result) {
+        done(); // closing the connection;
+        if(err){
+            console.log(err);
+            res.status(400).send(err);
+        }
+        //res.status(200).send(result.rows);
+        res.status(200).render('index', { title: 'ghstwrtr' });
+    });
   });
+
+  //res.render('index', { title: 'ghstwrtr' });
+});
 
 router.get('/login', (req, res) => {
     res.render('login', { title: 'ghstwrtr' });
 })
+
+
+
 
 module.exports = router;
