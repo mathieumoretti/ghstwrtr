e28053ea2c4971113c976330a84784a746f5ea61
Mathieu Moretti <mormmorm@hotmail.com>
2019-11-10 18:10:36 -0500

    First few units tests, utils unit tests barebone

---
+++ test/utils.test.js
@@ -0,0 +1,6 @@
+const test = require('tape');
+
+test('should return -1 when the value is not present in Array', function (t) {
+    t.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so passes
+    t.end();
+  });
