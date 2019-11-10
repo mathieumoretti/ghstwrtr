ea054c1da8bb0d7c07301eedc2943eff3310865b
Mathieu Moretti <mormmorm@hotmail.com>
2019-11-10 18:46:31 -0500

    Unit tests for existy and truthy

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,25 @@
 const test = require('tape');
 
-test('should return -1 when the value is not present in Array', function (t) {
-    t.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so passes
+const utils = require('../scripts/utils')
+
+test('should return true of false for various existy scenarios', function (t) {
+
+  // Add unit tests for existy
+    t.equal(false, utils.existy(null)); //=> false
+    t.equal(false, utils.existy(undefined)); //=> false
+    t.equal(false, utils.existy({}.notHere)); //=> false
+    t.equal(false, utils.existy((function(){})())); //=> false
+    t.equal(true , utils.existy(0)); //=> true
+    t.equal(true,  utils.existy(false)); //=> true 
     t.end();
+  });
+
+  test('should return true of false for various truthy scenarios', function (t) {
+
+    // Add unit tests for truthy
+      t.equal(false, utils.truthy(false)); //=> false
+      t.equal(false, utils.truthy(undefined)); //=> false
+      t.equal(true, utils.truthy(0)); //=> true
+      t.equal(true, utils.truthy('')); //=> true 
+      t.end();
   });
