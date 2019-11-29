add9f3323d9e6096aecf652f8ad2d60705a9946f
Mathieu Moretti <mormmorm@hotmail.com>
2019-11-28 23:48:49 -0500

    Primitive checkers

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,8 +1,11 @@
 const test = require('tape');
+const _ = require('underscore')
 
 const utils = require('../scripts/utils')
 
-test('should return true of false for various existy scenarios', function (t) {
+
+// existy
+test('should return true or false for various existy scenarios', function (t) {
     t.equal(false, utils.existy(null)); //=> false
     t.equal(false, utils.existy(undefined)); //=> false
     t.equal(false, utils.existy({}.notHere)); //=> false
@@ -12,7 +15,8 @@ test('should return true of false for various existy scenarios', function (t) {
     t.end();
   });
 
-  test('should return true of false for various truthy scenarios', function (t) {
+  // truthy
+  test('should return true or false for various truthy scenarios', function (t) {
       t.equal(false, utils.truthy(false)); //=> false
       t.equal(false, utils.truthy(undefined)); //=> false
       t.equal(true, utils.truthy(0)); //=> true
@@ -20,6 +24,8 @@ test('should return true of false for various existy scenarios', function (t) {
       t.end();
   });
 
+
+  // rootDir
   test('should find a root folder with name index.js', function (t) {
       // Add unit tests for truthy
       //t.equal(false, utils.truthy(false)); //=> false
@@ -28,3 +34,22 @@ test('should return true of false for various existy scenarios', function (t) {
       t.notEqual(undefined, rootDir)
       t.end();
   });
+
+  // checkers
+  test('should return true or false for various truthy scenarios', function (t) {
+    
+    var aVar = {}.a = 'a';
+
+    function always(VALUE) {
+        return function() {
+            return VALUE;
+        };
+    };
+
+    var never = always(false);
+    never.message = "It's false buddy";
+    var neverPasses = utils.checker(never); 
+
+    t.equal(1, neverPasses().length);
+    t.end();
+});
