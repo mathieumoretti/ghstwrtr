3d15ec15aa88c96731d4e56e298cd14fca8a6b64
Mathieu Moretti <mormmorm@hotmail.com>
2020-03-04 22:31:11 -0500

    Use it in fetchBooks

--- scripts/fetchBooks.js
+++ scripts/fetchBooks.js
@@ -1,10 +1,35 @@
 "use strict";
+const _ = require('underscore');
+var url = require('url');
 
 const fh = require('./fileHandler');
 const requester = require('./requester');
 const utils = require('./utils');
 
-var url = require('url');
+const commandLineArgs = require('command-line-args')
+
+const optionDefinitions = [   
+    { name: 'id', type: String, defaultOption: true },
+    { name: 'type', alias: 't', type: String }
+  ]
+
+const options = commandLineArgs(optionDefinitions)
+
+
+var id = 8;
+var type = "txt";
+if(utils.existy(options[id]))
+{
+    switch(options[id])
+    {
+        case "txt":            
+        case "epub":
+            type = options[id];
+            break;
+        default:break;
+    };
+}
+
 var logcontent = (x)=>
 {
     return utils.note(x.content);
@@ -17,14 +42,16 @@ promise.then((res)=>{
 
     // request data
     const baseUrl = "https://www.gutenberg.org/cache/epub";
-    const firstBookId = 8;
+    const firstBookId = id;
     const books = 1;
     for (let i = firstBookId; i < firstBookId + books ; i++) {
-        const adr = `${baseUrl}/${i}/pg${i}.txt`
+        const fileName = `pg.${type}`;
+        //const adr = `${baseUrl}/${i}/${fileName}.noimages`
+        const adr = "http://www.gutenberg.org/robot/harvest?filetypes[]=epub.noimages";
         var someUrl = url.parse(adr, true);
         requester.request(someUrl)
             .then(function(response){
-                    return fh.write(`${tmp}/pg${i}.txt`, response.body);
+                    return fh.write(`${tmp}/${fileName}`, response.body);
             }).then(logcontent, utils.fail);
     }
 });
