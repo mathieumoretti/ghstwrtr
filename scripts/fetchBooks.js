33a56862c7f1d1deeac61cb6385bc2628ac53ab7
mathieumoretti <mormmorm@hotmail.com>
2020-03-17 22:58:18 -0400

    Fetch books now can have an output directory cmd line (#66)
    * Fetch books now can have an output directory cmd line
    
    * bump to 10.12.0 node

--- scripts/fetchBooks.js
+++ scripts/fetchBooks.js
@@ -1,25 +1,30 @@
 "use strict";
 const _ = require('underscore');
-var url = require('url');
+const url = require('url');
+const commandLineArgs = require('command-line-args');
+const path = require('path');
+const fs = require('fs');
 
 const fh = require('./fileHandler');
 const requester = require('./requester');
-const iconv  = require('iconv-lite');
 const utils = require('./utils');
-
-const commandLineArgs = require('command-line-args')
 
 const optionDefinitions = [   
     { name: 'id', alias: 'i',type: String },
-    { name: 'type', alias: 't', type: String }
+    { name: 'type', alias: 't', type: String },
+    { name: 'outDir', alias: 'o', type: String }
   ]
 
 const options = commandLineArgs(optionDefinitions)
 
-
 var id = options.id || 8;
 var type = "txt";
-const books = 1;
+var rootDir = `${utils.rootDir}`;
+var outDir = path.join(`${rootDir}`, "tmp", "fetched");
+
+if (utils.existy(options.outDir)) { 
+    outDir = path.join(`${rootDir}`, `${options.outDir}`, "fetched");
+}
 
 if(utils.existy(options.type))
 {
@@ -41,9 +46,7 @@ var logcontent = (x)=>
     return utils.note(x.content);
 };
 
-const tmp = `${utils.rootDir}/tmp`;
-
-var promise = fh.mkdir2(tmp, true);
+var promise = fh.mkdir2(outDir, true);
 
 promise.then((res)=>{
 
@@ -57,11 +60,6 @@ promise.then((res)=>{
     var someUrl = url.parse(adr, true);
     requester.request(someUrl)
         .then(function(response){
-                var res = response.toJSON();
-
-                var utf8String = iconv.decode(new Buffer(response.body), "ISO-8859-1");
-                console.log(utf8String);
-
-                 return fh.write(`${tmp}/${fileName}`, response.body);
+                 return fh.write(`${outDir}/${fileName}`, response.body);
         }).then(logcontent, utils.fail);
 });
