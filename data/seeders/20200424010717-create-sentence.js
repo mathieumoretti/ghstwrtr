68a602c6e01f8daea0ae48b3132ceb586b11fa54
Mathieu Moretti <mormmorm@hotmail.com>
2020-04-23 23:55:45 -0400

    Seeding Bible sentences

--- data/seeders/20200424010717-create-sentence.js
+++ data/seeders/20200424010717-create-sentence.js
@@ -3,11 +3,8 @@
 const path = require('path');
 const _ = require('underscore');
 
-const Sentence = require('../models/sentence');
-
 const fh = require('../../scripts/fileHandler');
 const utils = require('../../scripts/utils');
-
 
 var rootDir = `${utils.rootDir}`;
 var inDir = path.join(`${rootDir}`, "tmp", "parsed");
@@ -15,6 +12,8 @@ const filename = `pg10900.txt`;
 const filepath = `${inDir}/${filename}`;
 
 var readPromise = fh.read(filepath);
+
+var verseRegex = /\d+:\d+\s/i;
 
 module.exports = {
   up : function (queryInterface, Sequelize) {
@@ -42,15 +41,18 @@ module.exports = {
           var data = result.content;
           var lines = data.split("\n")
           utils.note(lines[1]);
-          var sentences = [
-          { content: lines[1],
-            createdAt : new Date(),
-            updatedAt : new Date(),
-          }];         
-          
-          //var lines = data.split("\n");
-          //utils.note(lines[0]); 
-          return queryInterface.bulkInsert("Sentences", sentences);
+          var sentences = _.map(lines, (line)=>{
+            return { content: line.trim(),
+              createdAt : new Date(),
+              updatedAt : new Date(),
+            };
+          });
+          var filteredSentences = _.filter(sentences, (s)=>{return s.content !== ""});
+          filteredSentences = _.map(filteredSentences, (sentence)=>{
+            sentence.content = sentence.content.replace(verseRegex,"");
+            return sentence;
+          })
+          return queryInterface.bulkInsert("Sentences", filteredSentences);
       });
   },
 
@@ -62,6 +64,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
-   return promiseMaker.make(()=>{utils.note("What down?")})
+   return queryInterface.bulkDelete('Sentences', null, {});
   }
 };
