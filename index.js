40c966347e7e5fd4067e885e9a362c71f7bab973
Mathieu Moretti <mormmorm@hotmail.com>
2020-04-29 18:54:19 -0400

    Controller refactor + model

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const console = require('console');
 const path = require('path');
-// const _ = require('underscore');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,18 +11,14 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, '/public')));
 
-
 // add routes
 const mainController = require('./controllers/mainController');
 const sentenceController = require('./controllers/sentenceController');
-const storyController = require('./controllers/storyController');
 const storiesController = require('./controllers/storiesController');
 
 app.use('/', mainController);
 app.use('/sentence', sentenceController);
-app.use('/story', storyController);
 app.use('/stories', storiesController);
-
 
 app.listen(PORT);
 console.log(`Server started on port ${PORT}`);
