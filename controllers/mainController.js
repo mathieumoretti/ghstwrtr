ec4636376e45666a1d7cfd400bb7c5b861e9dc94
Mathieu Moretti <mormmorm@hotmail.com>
2020-05-12 23:44:22 -0400

    page views as a hello world

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,11 +1,22 @@
 const express = require('express');
 var router = express.Router();
 
-
-
 router.get('/', (req, res) => {
-    res.render('index', { title: 'ghstwrtr' });
+  let page_views = 0;
+  if(req.session.page_views){
+    page_views = req.session.page_views++;    
+  } else {
+    page_views =  req.session.page_views = 1;
+  }
+  
+  res.render('index', {
+    model:
+    {
+      title: 'ghstwrtr',
+      page_views: page_views
+    },
   });
+});
 
 // router.get('/', isLoggedIn, function(req, res) {
 //     res.sendFile(path.join(__dirname, '../', 'public', 'html', 'dashboard.html'), {
--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require('express');
 const console = require('console');
 const path = require('path');
 
+
+
+const redis = require("redis");
+const session = require('express-session');
+var redisStore = require('connect-redis')(session);
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+//Create Redis client on Redis port
+const redisClient = redis.createClient(REDIS_PORT);
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,12 +21,34 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, '/public')));
 
+// sesh
+app.use(session({
+    secret: 'sesshhhhh',
+    // create new redis store.
+    store: new redisStore({ host: 'localhost', port: REDIS_PORT, client: redisClient, ttl :  260}),
+    saveUninitialized: false,
+    resave: false
+}));
+
+function IsLoggedIn(req, res, next) {
+    console.log('Time:', Date.now());
+    // create new session object.
+    if(!req.session)
+    {
+        res.render("error", {model:{err: "SUp error"}});
+    }
+    console.log(req.session);
+    next();
+}
+
+app.use("/", IsLoggedIn);
+
 // add routes
 const mainController = require('./controllers/mainController');
 const sentenceController = require('./controllers/sentenceController');
 const storiesController = require('./controllers/storiesController');
 
-app.use('/', mainController);
+app.use('/', IsLoggedIn, mainController);
 app.use('/sentence', sentenceController);
 app.use('/stories', storiesController);
 
