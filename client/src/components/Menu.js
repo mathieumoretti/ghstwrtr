263037e69d5e3cfc318aca0e6b142d0b605040b3
Mathieu Moretti <mormmorm@hotmail.com>
2020-07-13 21:29:07 -0400

    And more...

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -20,7 +20,7 @@ export class Menu extends Component {
               <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/store.html">Link</a>
+              <a className="nav-link" href="/store.html">Store</a>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -36,11 +36,12 @@ export class Menu extends Component {
             <li className="nav-item">
               <a className="nav-link disabled" href="#">Disabled</a>
             </li>
+            <li className="nav-item">
+              <span className="glyphicon glyphicon-piggy-bank text-center"></span>
+              <span class="badge badge-dark">9</span>
+            </li>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+          
         </div>
       </nav>
         );
