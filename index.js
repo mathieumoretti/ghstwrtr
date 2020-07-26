e352a7484f6f685a1fa7207ad14df52ebf73e33c
Mathieu Moretti <mormmorm@hotmail.com>
2020-07-26 14:28:19 -0400

    Log in helloworld

--- index.js
+++ index.js
@@ -5,18 +5,50 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+//redis
+const redis = require("redis");
+const session = require('express-session');
+var redisStore = require('connect-redis')(session);
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+//Create Redis client on Redis port
+const redisClient = redis.createClient(REDIS_PORT);
+
+// sesh
+app.use(session({
+  secret: 'sesshhhhh',
+  // create new redis store.
+  store: new redisStore({ host: 'localhost', port: REDIS_PORT, client: redisClient, ttl :  260}),
+  saveUninitialized: false,
+  resave: false
+}));
+
 // add routes
 const storyController = require('./controllers/storyController');
 const sentenceController = require('./controllers/sentenceController');
 
-// views - not sure
+
+function IsLoggedIn(req, res, next) {
+  console.log('Time:', Date.now());
+  // create new session object.  
+  if(!req.session)
+  {
+    res.sendFile(path.join(path.resolve(__dirname, 'dist'), 'login.html'));
+  }
+  console.log(req.session);
+  next();
+}
+
+app.use("/", IsLoggedIn);
+
+// views
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/api/stories', storyController);
 app.get('/api/sentences', sentenceController);
 
 // send the user to index html page inspite of the url
-app.get('/', (req, res) => {
+app.get('/', IsLoggedIn, (req, res) => {
   res.sendFile(path.join(path.resolve(__dirname, 'dist'), 'app.html'));
 });
 
