15b02676d3c3af73f2843ed7b3c445a196e24af4
Mathieu Moretti <mormmorm@hotmail.com>
2020-10-04 12:06:31 -0400

    multiple dimension ngrams

---
+++ client/src/components/Workbench.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import nlp from "compromise";
+
+function ngrams(array, length) {
+  var ngramsArray = [];
+
+  for (var i = 0; i < array.length - (length - 1); i++) {
+      var subNgramsArray = [];
+
+      for (var j = 0; j < length; j++) {
+          subNgramsArray.push(array[i + j])
+      }
+
+      ngramsArray.push(subNgramsArray);
+  }
+
+  return ngramsArray;
+}
+
+function multipleNgrams(array, minLength, maxLength)
+{
+  var ngramsArray = [];
+
+  if (maxLength < minLength)
+    return ngramsArray;
+
+  for (var i = minLength; i < maxLength; i++)
+  {
+    ngramsArray.push(...ngrams(array, i));
+  }
+
+  return ngramsArray;
+}
+
+export class Workbench extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      fragments: null
+    };
+  }
+
+  componentDidMount() {
+    // Simple GET request using fetch
+    fetch('api/sentences')
+        .then(response => response.json())
+        .then(data => {          
+          let doc = nlp(data.sentences.join(' '));
+          let sentences = doc.sentences();
+          this.setState({ fragments: sentences })
+          this.setState({ loading: false })
+        }).catch((e)=>{
+          console.log("alice is mad:" + e);
+        });
+  }
+  handleClick(){}
+  render() {
+
+    if (this.state.loading)
+    {
+      return <div>loading Workbench...</div>;
+    }
+
+    let frag = this.state.fragments.list;
+    let ngram = multipleNgrams(frag[0].terms(), 5, 9);
+    console.log(ngram);
+
+    return (<ErrorBoundary>
+      {<div>{
+          <div>
+            {
+              ngram.map(
+                (val,index)=>{
+                  return <div key={index}>{ 
+                      val.map((x)=>x.text).join(' ')
+                    }</div>
+                })
+            }
+          </div>
+        }</div>
+      }</ErrorBoundary>
+    );
+  }
+}
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Mettez à jour l'état, de façon à montrer l'UI de repli au prochain rendu.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Vous pouvez aussi enregistrer l'erreur au sein d'un service de rapport.
+    //logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Vous pouvez afficher n'importe quelle UI de repli.
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
