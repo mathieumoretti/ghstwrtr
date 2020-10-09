12106960349d02b6d971870569bd0736ae2faac6
Mathieu Moretti <mormmorm@hotmail.com>
2020-10-08 23:24:54 -0400

    Moving ErrorBoundary into its own file

--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Sentence } from "./Sentence";
 
+import { Sentence } from "./Sentence";
+import {ErrorBoundary} from "./ErrorBoundary";
 
 export class Store extends React.Component {
   constructor(props) {
@@ -45,31 +46,5 @@ export class Store extends React.Component {
       </div>
       }</ErrorBoundary>
     );
-  }
-}
-
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    // Mettez à jour l'état, de façon à montrer l'UI de repli au prochain rendu.
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    // Vous pouvez aussi enregistrer l'erreur au sein d'un service de rapport.
-    //logErrorToMyService(error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      // Vous pouvez afficher n'importe quelle UI de repli.
-      return <h1>Something went wrong.</h1>;
-    }
-
-    return this.props.children;
   }
 }
